b1d192912152d9b993d9abcf5f5afa01204b57ca
Pablo <pablo.s.perez@hotmail.com>
2020-10-10 15:22:16 -0300

    Terminado

--- ejercicio_6/app.js
+++ ejercicio_6/app.js
@@ -1,5 +1,24 @@
 
 const obtenerPersonaFake = require("./library");
 
+const mostrarPersona = function (res){
+    const persona = res.results[0];
+    console.log('Nombre: ', persona.name.first, persona.name.last);
+    console.log('Género: ', persona.gender);
+    console.log('País: ', persona.location.country);
+    console.log('Estado: ', persona.location.state);
+    console.log('Ciudad: ', persona.location.city);
+    console.log('Dirección: ', persona.location.street.name, persona.location.street.number);
+    console.log('Código postal: ', persona.location.postcode);
+    console.log('Email: ', persona.email);
+    console.log('Nombre de usuario: ', persona.login.username);
+    console.log('Password: ', persona.login.password);
+}
+
 // Usar la funcion obtenerPersonaFake() la cual devuelve la promesa de traer el objeto persona extraido
-obtenerPersonaFake().then(x => console.log(x));
+obtenerPersonaFake()
+.then((persona)  => {
+ mostrarPersona(persona);
+}).catch((err) => {
+    console.log("No se encontro la persona", err);
+});
