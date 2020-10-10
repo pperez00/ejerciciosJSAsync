0f7e0cad836472fc8b8c0e06d8d4d073257dadf3
Pablo <pablo.s.perez@hotmail.com>
2020-10-10 15:07:42 -0300

    Ej 5 resuelto

--- ejercicio_5/app.js
+++ ejercicio_5/app.js
@@ -3,8 +3,18 @@ const obtenerPersonaFake = require("./library");
 
 // Codigo funcion callback
 
-function callback(nombre){
-    console.log(nombre);
+function callback(res){
+    const persona = res.results[0];
+    console.log('Nombre: ', persona.name.first, persona.name.last);
+    console.log('Género: ', persona.gender);
+    console.log('País: ', persona.location.country);
+    console.log('Estado: ', persona.location.state);
+    console.log('Ciudad: ', persona.location.city);
+    console.log('Dirección: ', persona.location.street.name, persona.location.street.number);
+    console.log('Código postal: ', persona.location.postcode);
+    console.log('Email: ', persona.email);
+    console.log('Nombre de usuario: ', persona.login.username);
+    console.log('Password: ', persona.login.password);
 }
 
 // Fin codigo
